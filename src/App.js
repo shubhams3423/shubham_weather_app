40c9be124cf49dd44dfbefdad664cd9d71735ebf
shubhams3423 <shubham1844s@gmail.com>
2023-08-16 19:24:05 +0530

    fetch.catch added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   fetch("http://api.weatherapi.com/v1/current.json?key=5bca6bccf3ce4c41a8f15340232807&q=Pune")
     .then((response) => response.json())
     .then((data) => console.log(data))
+    .catch((error) => console.log(error))
   return (
     <div className="App">
       <h1>Weather App</h1>
