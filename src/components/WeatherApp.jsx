89197251e96e5125cb7f5b1f9757ba4e2bdb2256
shubhams3423 <shubham1844s@gmail.com>
2023-08-17 19:07:04 +0530

    Weather app created

---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { FiSearch } from 'react-icons/fi'
+import { BsDropletFill } from 'react-icons/bs'
+import { FaWind } from 'react-icons/fa'
+const WeatherApp = () => {
+    return (
+        <div >
+            <main>
+                <div className="weatherApp" >
+                    <div className="d-flex align-items-center">
+                        <input type="text" className='inputText' placeholder='Pune' />
+                        <div className='searchIcon'>
+                            <FiSearch />
+                        </div>
+                    </div>
+                    <div >
+                        <img src="https://previews.123rf.com/images/martialred/martialred1702/martialred170200035/71710170-cloudy-or-cloud-partly-blocking-the-sun-flat-color-vector-icon-for-weather-apps-and-websites.jpg" alt="weather Img" className='img-fluid weatherImg' />
+                    </div>
+                    <div>
+                        <div className="temperature text-center">
+                            <h2>28 &#8451;</h2>
+                            <p>Scattered Clouds</p>
+                        </div>
+                        <div className='d-flex justify-content-between endSection'>
+                            <div className="d-flex align-items-center">
+                                <div>
+                                    <BsDropletFill size={31} />
+                                </div>
+                                <div className='ms-2'>
+                                    <p className='fw-bold '>51%</p>
+                                    <span className='fw-medium'>Humidity</span>
+                                </div>
+                            </div>
+                            <div className=" d-flex align-items-center  ">
+                                <div>
+                                    <FaWind size={31} />
+                                </div>
+                                <div className='ms-2'>
+                                    <p className='fw-bold'>4.63Km/H</p>
+                                    <span className='fw-medium'>Wind Speed</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default WeatherApp
