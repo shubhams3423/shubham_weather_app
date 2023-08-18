afbbb1c98f86876edc00027c12ccb269fc3d27d4
shubhams3423 <shubham1844s@gmail.com>
2023-08-18 13:06:25 +0530

    Weather data fetched in Weather App component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.css';
 import WeatherApp from './components/WeatherApp';
 
 function App() {
-  fetch("http://api.weatherapi.com/v1/current.json?key=5bca6bccf3ce4c41a8f15340232807&q=Pune")
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
   return (
     <div className="App">
       <WeatherApp />
