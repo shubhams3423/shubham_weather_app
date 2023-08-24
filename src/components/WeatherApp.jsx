f4f3dab071a1ce392afc8795a5c8d9c8c7e0ce2c
shubhams3423 <shubham1844s@gmail.com>
2023-08-24 07:53:09 +0530

    feat/search text place

--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -3,29 +3,42 @@ import BeatLoader from "react-spinners/BeatLoader";
 
 
 import { FiSearch } from 'react-icons/fi'
-import { BsDropletFill } from 'react-icons/bs'
+// import { BsDropletFill } from 'react-icons/bs'
 import { FaWind } from 'react-icons/fa'
 const WeatherApp = () => {
     const [weatherData, setWeatherData] = useState({})
     const [isLoading, setIsLoading] = useState(false)
+    const [inputName, setInputName] = useState("");
+    const [saveInputText, setSaveInputText] = useState("Pune");
+
     useEffect(() => {
         setIsLoading(true);
-        fetch("https://api.weatherapi.com/v1/current.json?key=5bca6bccf3ce4c41a8f15340232807&q=Pune")
+        fetch(`https://api.weatherapi.com/v1/current.json?key=5bca6bccf3ce4c41a8f15340232807&q=${saveInputText}`)
             .then((response) => response.json())
             .then((data) => {
                 setWeatherData(data)
+
                 setIsLoading(false)
             })
             .catch((error) => console.log(error))
-    }, [])
+    }, [saveInputText])
+    const checkOnKeyDown = (e) => {
+        if (e.key === "Enter") {
+            setSaveInputText(inputName)
+        }
+    }
+    const onClickRenderWeather = () => {
+        if (inputName !== "")
+            setSaveInputText(inputName)
+    }
     return (
         <div >
             <main>
                 <div className="weatherApp" >
                     <div className="searchSection">
-                        <input type="text" className='inputText' placeholder='Pune' />
+                        <input type="text" className='inputText' placeholder='Search...' value={inputName} onChange={(e) => setInputName(e.target.value)} onKeyDown={checkOnKeyDown} />
                         <div className='searchIcon'>
-                            <FiSearch size={19} />
+                            <FiSearch size={19} onClick={onClickRenderWeather} />
                         </div>
                     </div>
                     <div >
@@ -42,7 +55,11 @@ const WeatherApp = () => {
                         <div className='lastSection'>
                             <div className="weatherSection">
                                 <div>
-                                    <BsDropletFill size={31} />
+                                    {isLoading ? <BeatLoader
+                                        color="#252827"
+                                        size={10}
+                                    /> : weatherData?.current?.humidity <= 55 ? "Dry and Comfortable" : (weatherData?.current?.humidity <= 65 && weatherData?.current?.humidity > 55) ? "Muggy" : "Moisture"}
+                                    {/* <BsDropletFill size={31} /> */}
                                 </div>
                                 <div>
                                     <p>{isLoading ? <BeatLoader
