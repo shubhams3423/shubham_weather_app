218b894f40f3c266d4f1b03c81c25d936d457181
shubhams3423 <shubham1844s@gmail.com>
2024-02-15 16:19:07 +0530

    added social icon.

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BiLogoGmail } from "react-icons/bi";
 import { AiFillGithub } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
 import "../ComponentCss/ContactUs.css";
 const ContactUs = ({ theme }) => {
   return (
@@ -8,7 +9,10 @@ const ContactUs = ({ theme }) => {
       <div
         className={`socialMediaIconContainer socialMediaIconContainer-${theme}`}
       >
-        <a href="https://github.com/shubhams3423/shubham_weather_app">
+        <a
+          href="https://github.com/shubhams3423/shubham_weather_app"
+          target="_blank"
+        >
           <AiFillGithub
             className={`socialMediaIcons-${theme} socialMediaIcons `}
           />
@@ -17,8 +21,20 @@ const ContactUs = ({ theme }) => {
       <div
         className={`socialMediaIconContainer socialMediaIconContainer-${theme}`}
       >
-        <a href="mailto :shubham1844s@gmail.com" target="_blank">
+        <a href="">
           <BiLogoGmail
+            className={`socialMediaIcons-${theme} socialMediaIcons`}
+          />
+        </a>
+      </div>
+      <div
+        className={`socialMediaIconContainer socialMediaIconContainer-${theme}`}
+      >
+        <a
+          href="https://www.linkedin.com/in/shubham-shinde-69b688297/"
+          target="_blank"
+        >
+          <FaLinkedin
             className={`socialMediaIcons-${theme} socialMediaIcons`}
           />
         </a>
